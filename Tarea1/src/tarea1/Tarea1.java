edc3e4ed597c1fa498f55c924096c209b8e1f943
Dvillanea <dvillanea1521@gmail.com>
2016-02-11 22:28:17 -0600

    Update Tarea1.java

--- Tarea1/src/tarea1/Tarea1.java
+++ Tarea1/src/tarea1/Tarea1.java
@@ -17,6 +17,7 @@ public class Tarea1 {
     public static void main(String[] args) {
         System.out.println("hola Mundo");
         System.out.println("Es una prueba");
+        System.out.println("modificado desde git");
     }
     
 }
